0442e328c71683ab62a43a6eaed421d7b90ae3c3
LoHBot <lohbot@gmail.com>
2016-09-03 20:57:10 -0300

    Just version number update

--- LoHBot/My Project/AssemblyInfo.vb
+++ LoHBot/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2.2016.7.16")>
-<Assembly: AssemblyFileVersion("2.2016.7.16")>
+<Assembly: AssemblyVersion("2.2016.9.3")>
+<Assembly: AssemblyFileVersion("2.2016.9.3")>
